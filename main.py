130dd8085af726bef52a165d3e40dde9124a46d7
enbyautumn <60362123+enbyautumn@users.noreply.github.com>
2020-02-03 14:59:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ ds = SupervisedDataSet(2, 1)
 
 data = open("/Users/mac/data.json", "r").read()
 data = json.loads(data)
-for x in len(data): 
+for x in range(len(data)): 
 	ds.addSample((data[x]["main"]["humidity"], data[x]["main"]["humidity"]), (data[x + 1]["main"]["temp"]))
 	
 
