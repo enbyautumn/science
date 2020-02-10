d2551dd340cd7a97a7c2c5affc0e51c402ee6274
iliketocode2 <60369048+iliketocode2@users.noreply.github.com>
2020-02-10 15:08:03 -0500

    Update WIPGraphics.py

--- WIPGraphics.py
+++ WIPGraphics.py
@@ -5,7 +5,7 @@ x = np.linspace(-1, 3 * np.pi, 2)
 y = np.exp(np.sin(x))
 
 plt.stem(x, y, use_line_collection=True)
-plt.show()
+
 
 #The first number controls the initial hieght of the graph, and the second number 
 #after the comma controls the frquency of the graph. Finally, the third number after the np.pi, 
@@ -15,7 +15,7 @@ plt.show()
 markerline, stemlines, baseline = plt.stem(
     x, y, linefmt='grey', markerfmt='D', bottom=1.1, use_line_collection=True)
 markerline.set_markerfacecolor('none')
-plt.show()
+
 
 
 #This controls the shape and color of the data point and it also creates a "baseline" that the graph can go above or below
@@ -35,7 +35,7 @@ ax.scatter(2, 3)
 ax.legend()
 ax.grid(True)
 
-plt.show()
+
 
 # n controls the number of poits on the graph, the x,y controls the x and y coordinates, to simply plot a point, ax.scatter (x, y)
 
