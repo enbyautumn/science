416a6c1b6ce77a3a56d7e3ce25319879f776379d
enbyautumn <60362123+enbyautumn@users.noreply.github.com>
2020-02-10 14:35:04 -0500

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,1 +1,12 @@
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
+from pybrain.supervised.trainers import BackpropTrainer
+import simplejson as json
+import math
+import requests, json 
+
+
+net = NetworkReader.readFrom('.xml') 
 
