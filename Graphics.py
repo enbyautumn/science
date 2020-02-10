6428cc7f4601d3761c7efe252a01c942f83886ad
iliketocode2 <60369048+iliketocode2@users.noreply.github.com>
2020-02-10 14:46:26 -0500

    Update Graphics.py

--- Graphics.py
+++ Graphics.py
@@ -19,3 +19,22 @@ plt.show()
 
 
 #This controls the shape and color of the data point and it also creates a "baseline" that the graph can go above or below
+
+import numpy as np
+np.random.seed(1)
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots()
+for color in ['green']:
+    x, y = 5, 4
+    scale = 200.0 * np.random.rand(n)
+    ax.scatter(x, y, c=color, s=scale, label=color,
+               alpha=0.3, edgecolors='none')
+ax.scatter(2, 3)
+ax.legend()
+ax.grid(True)
+
+plt.show()
+
+# n controls the number of poits on the graph, the x,y controls the x and y coordinates, to simply plot a point, ax.scatter (x, y)
