fb80557b61dfb2ef321fe345ea500b8133c3bd67
iliketocode2 <60369048+iliketocode2@users.noreply.github.com>
2020-02-12 17:32:30 -0500

    Update WIPGraphics.py

--- WIPGraphics.py
+++ WIPGraphics.py
@@ -9,3 +9,22 @@ ax.grid(True)
 
 
 plt.show()
+
+#This could be interesting to use (an animation):
+def run(data):
+    # update the data
+    t, y = data
+    xdata.append(t)
+    ydata.append(y)
+    xmin, xmax = ax.get_xlim()
+
+    if t >= xmax:
+        ax.set_xlim(xmin, 2*xmax)
+        ax.figure.canvas.draw()
+    line.set_data(xdata, ydata)
+
+    return line,
+
+ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
+                              repeat=False, init_func=init)
+plt.show()
