6b66e9a6cff24b1f282f28d29541b79444d5328d
iliketocode2 <60369048+iliketocode2@users.noreply.github.com>
2020-02-13 15:32:32 -0500

    Update ActualGraphics.py

--- ActualGraphics.py
+++ ActualGraphics.py
@@ -1,19 +1,23 @@
 import matplotlib
 matplotlib.use('PS')
-
 import matplotlib.pyplot as plt
 from numpy.random import rand
 
+net = NetworkReader.readFrom('/Users/mac/weights.xml') 
+data = open("/Users/mac/data.json", "r").read()
+data = json.loads(data)
 
-fig, ax = plt.subplots()
-   
-ax.scatter(2, 5)
+x = random.randrange(0, 1000)
+print("expected: " + str(data[x+1]["main"]["temp"]))
+
+ax.scatter("temp", )
 ax.grid(True)
 
 ax.set_xlabel('Days', fontsize=15)
 ax.set_ylabel('Temperature', fontsize=15)
 ax.set_title('Temperature Chart in Degrees Farenhite')
 
+fig, ax = plt.subplots()
 ax.grid(True)
 plt.show()
 
