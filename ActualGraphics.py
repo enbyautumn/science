097a36c57f0516e13616b9ab13315165ffff4f4f
iliketocode2 <60369048+iliketocode2@users.noreply.github.com>
2020-02-13 15:29:56 -0500

    Update ActualGraphics.py

--- ActualGraphics.py
+++ ActualGraphics.py
@@ -1,1 +1,20 @@
+import matplotlib
+matplotlib.use('PS')
+
+import matplotlib.pyplot as plt
+from numpy.random import rand
+
+
+fig, ax = plt.subplots()
+   
+ax.scatter(2, 5)
+ax.grid(True)
+
+ax.set_xlabel('Days', fontsize=15)
+ax.set_ylabel('Temperature', fontsize=15)
+ax.set_title('Temperature Chart in Degrees Farenhite')
+
+ax.grid(True)
+plt.show()
+
 
