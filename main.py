f681b563e36999ae3293702bb5411197bfd57dd8
enbyautumn <60362123+enbyautumn@users.noreply.github.com>
2020-03-08 12:15:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -119,5 +119,5 @@ trainer = BackpropTrainer(net, ds, verbose=True)
 	start = time.time()
 """
 trainer = BackpropTrainer(net, ds, verbose=True)
-trainer.trainUntilConvergence(maxEpochs=50, verbose=True)
+trainer.trainUntilConvergence(maxEpochs=10000, verbose=True)
 NetworkWriter.writeToFile(net, 'Z:\\weights.xml') 
